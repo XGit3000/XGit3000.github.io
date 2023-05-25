60a9c5c33a56b3a1f09969c4cccbeb620b3f041d
XGit3000 <sebastian.carrier@gmail.com>
2023-05-25 04:54:39 -0400

    Added some JS to actually save the options yay

--- nuclearshopbase.js
+++ nuclearshopbase.js
@@ -2,7 +2,7 @@ function fillMenu(titletext,itemtexts,targetdiv,titleclassname,itemclassname,men
     let newdivstart = `<button class="${titleclassname}"><p class="ButtonTitleText">${titletext}</p>`;
     let newmenuitemsdiv = "";
     for(let i = 0; i<itemtexts.length; i++){
-        newmenuitemsdiv += `<label class="ItemText"><input type="radio" name=${titletext} />${itemtexts[i]}</label>`
+        newmenuitemsdiv += `<label class="ItemText"><input type="radio" name=${titletext} value=${itemtexts[i]} onclick="saveRadioOption('${titletext}')"/>${itemtexts[i]}</label>`
         // newmenuitemsdiv += `<input  type="radio" id="${itemtexts[i]}" name="${titletext}" value="${itemtexts[i]}"><label for="${itemtexts[i]}" class="ItemText">${itemtexts[i]}</label><br>`
         // newmenuitemsdiv += `<button class="${itemclassname}"><p class="ItemText">${itemtexts[i]}</p></button>`
     }
@@ -35,4 +35,15 @@ function initializeCollapsableMenus(){
                     content.style.maxHeight = content.scrollHeight + "px";}
             })
         }
+}
+
+function saveRadioOption(name){
+    let radios = document.getElementsByName(name);
+    for(let i = 0; i<radios.length; i++){
+        if (radios[i].checked){
+            localStorage.setItem(name, radios[i].value)
+            console.log("Saving "+name + " " +radios[i].value)
+            return radios[i].value
+        }
+    }
 }
