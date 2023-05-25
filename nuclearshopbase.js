5207ac242bbe08c609e0ef249fbc201d76a56807
XGit3000 <sebastian.carrier@gmail.com>
2023-05-24 20:19:58 -0400

    Got all the selection UI done heck yeah

--- nuclearshopbase.js
+++ nuclearshopbase.js
@@ -2,7 +2,9 @@ function fillMenu(titletext,itemtexts,targetdiv,titleclassname,itemclassname,men
     let newdivstart = `<button class="${titleclassname}"><p class="ButtonTitleText">${titletext}</p>`;
     let newmenuitemsdiv = "";
     for(let i = 0; i<itemtexts.length; i++){
-        newmenuitemsdiv += `<button class="${itemclassname}"><p class="ItemText">${itemtexts[i]}</p></button>`
+        newmenuitemsdiv += `<label class="ItemText"><input type="radio" name=${titletext} />${itemtexts[i]}</label>`
+        // newmenuitemsdiv += `<input  type="radio" id="${itemtexts[i]}" name="${titletext}" value="${itemtexts[i]}"><label for="${itemtexts[i]}" class="ItemText">${itemtexts[i]}</label><br>`
+        // newmenuitemsdiv += `<button class="${itemclassname}"><p class="ItemText">${itemtexts[i]}</p></button>`
     }
 
     // let newdiv = newdivstart + newmenuitemsdiv + `</button>`
